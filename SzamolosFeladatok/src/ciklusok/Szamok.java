8e028d24b0464fbea2990b749f0643ee1bbcddc4
Asztrik <asztrikpetrovai@gmail.com>
2023-03-02 13:44:37 +0100

    ciklus nemugyanaz a szám

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -3,15 +3,19 @@ package ciklusok;
 public class Szamok {
 
     public static void main(String[] args) {
-        for (int k = 2; k <= 4; k++) {
-            
-        
-        for (int j = 2; j <= 4; j++) {
+        int variaciougyanaz = 0;
 
-            for (int i = 2; i <= 4; i++) {
-                System.out.println(k +""+ j + "" + i);
+        for (int k = 2; k <= 4; k++) {
+            for (int j = 2; j <= 4; j++) {
+                for (int i = 2; i <= 4; i++) {
+                    if ((j != k && j!=i) && (i != k && i != j)) {
+                        System.out.println(k + "" + j + "" + i);
+                        variaciougyanaz++;
+                    }
+                }
             }
         }
-        }
+        System.out.println(variaciougyanaz + "-nyi variáció ha ugyanazt nem nézük");
+
     }
 }

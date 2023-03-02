ab081e4abbadfe2eaf2e57f6939a3157daca98e3
Asztrik <asztrikpetrovai@gmail.com>
2023-03-02 13:17:41 +0100

    számok összetétele

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,8 +1,17 @@
-
 package ciklusok;
 
 public class Szamok {
-     public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        for (int k = 2; k <= 4; k++) {
+            
+        
+        for (int j = 2; j <= 4; j++) {
+
+            for (int i = 2; i <= 4; i++) {
+                System.out.println(k +""+ j + "" + i);
+            }
+        }
+        }
     }
 }
